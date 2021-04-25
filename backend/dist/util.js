8571aab90efcc2dd362908ac04ad5a8c2ada9f6f
Desislav Ivanov <udie4we@abv.bg>
2021-04-25 11:31:02 +0200

    Fix ts issues

--- backend/dist/util.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-var sleep = function (ms) { return new Promise(function (r) { return setTimeout(r, ms); }); };
-exports.sleep = sleep;
